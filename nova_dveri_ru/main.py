0843c9aed053cb31c2a86774e6a4a915ba19434a
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-07 12:30:11 +0400

    Added 'def get_links_from_sitemaps(site_maps)'

--- nova_dveri_ru/main.py
+++ nova_dveri_ru/main.py
@@ -1,12 +1,15 @@
+from pprint import pprint
 from urllib.parse import urljoin
 
 from nova_dveri_ru.data import BASE_URL
-from nova_dveri_ru.sitemap.site_map import get_site_maps
+from nova_dveri_ru.sitemap.site_map import get_links_to_sitemaps,\
+    get_links_from_sitemaps
 
 
 def main():
-    site_maps = get_site_maps(urljoin(BASE_URL, 'robots.txt'))
-    print(site_maps)
+    maps = get_links_to_sitemaps(urljoin(BASE_URL, 'robots.txt'))
+    links = list(get_links_from_sitemaps(maps))
+    pprint(links)
 
 
 if __name__ == '__main__':
--- nova_dveri_ru/sitemap/site_map.py
+++ nova_dveri_ru/sitemap/site_map.py
@@ -1,16 +1,39 @@
+from pprint import pprint
 from urllib.parse import urljoin
 from urllib.robotparser import RobotFileParser
+
+import requests
+from lxml import etree
 
 from nova_dveri_ru.data import BASE_URL
 
 
-def get_site_maps(url) -> list:
+def get_links_to_sitemaps(robots_url) -> list:
+    """
+    Возвращает ссылки на все xml карты сайта
+    :param robots_url: robots.txt link
+    :return: список ссылок на карты сайта
+    """
     rp = RobotFileParser()
-    rp.set_url(url)
+    rp.set_url(robots_url)
     rp.read()
     return rp.site_maps()
 
 
+def get_links_from_sitemaps(site_maps):
+    """
+    Возвращает все ссылки со всех карт сайта
+    :param site_maps: список ссылок на карты сайта
+    :return: yield -> ссылка на страницу
+    """
+    for url in site_maps:
+        response = requests.get(url)
+        root = etree.fromstring(response.content)
+        for element in root:
+            yield element.getchildren()[0].text
+
+
 if __name__ == '__main__':
-    site_maps = get_site_maps(urljoin(BASE_URL, 'robots.txt'))
-    print(site_maps)
+    maps = get_links_to_sitemaps(urljoin(BASE_URL, 'robots.txt'))
+    links = list(get_links_from_sitemaps(maps))
+    pprint(links)
