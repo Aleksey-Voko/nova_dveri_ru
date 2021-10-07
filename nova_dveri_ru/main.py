eca22c4e7abbaca182508b0157631eab05f18450
Aleksey Voko <voko.aleksey@gmail.com>
2021-10-07 10:48:09 +0400

    Added 'get_site_maps(url) -> list'

--- nova_dveri_ru/main.py
+++ nova_dveri_ru/main.py
@@ -1,10 +1,12 @@
+from urllib.parse import urljoin
+
 from nova_dveri_ru.data import BASE_URL
-from nova_dveri_ru.sitemap.site_map import get_robots
+from nova_dveri_ru.sitemap.site_map import get_site_maps
 
 
 def main():
-    robot = get_robots(f'{BASE_URL}robots.txt')
-    print(robot)
+    site_maps = get_site_maps(urljoin(BASE_URL, 'robots.txt'))
+    print(site_maps)
 
 
 if __name__ == '__main__':
--- nova_dveri_ru/sitemap/site_map.py
+++ nova_dveri_ru/sitemap/site_map.py
@@ -1,12 +1,16 @@
-import requests
+from urllib.parse import urljoin
+from urllib.robotparser import RobotFileParser
 
 from nova_dveri_ru.data import BASE_URL
 
 
-def get_robots(url):
-    return requests.get(url).text
+def get_site_maps(url) -> list:
+    rp = RobotFileParser()
+    rp.set_url(url)
+    rp.read()
+    return rp.site_maps()
 
 
 if __name__ == '__main__':
-    robot = get_robots(f'{BASE_URL}robots.txt')
-    print(robot)
+    site_maps = get_site_maps(urljoin(BASE_URL, 'robots.txt'))
+    print(site_maps)
